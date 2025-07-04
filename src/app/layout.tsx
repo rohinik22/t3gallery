ec81f14cdd43039e8b5603fb330452e755d2f00a
rohinik22 <rohinink182@gmail.com>
2025-07-04 12:34:20 +0530

    sacffold UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,16 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav(){
+  return (
+    <nav className="flex items-center justify-between w-full p-4 text-xl font-semibold border-b">
+      <div>Gallery</div>
+
+      <div>Sign In</div>
+    </nav>
+  );
+}
+
 const geist = Geist({
   subsets: ["latin"],
   variable: "--font-geist-sans",
@@ -18,8 +28,8 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geist.variable}`}>
-      <body>{children}</body>
+    <html lang="en" className={`${geist.variable} flex flex-col gap-4`}>
+      <body className={`font-sans ${geist.variable}`}><TopNav />{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://fwondivlfr.ufs.sh/f/xBKBguy6vNcXCsYMA2vCtHIADJbl7UgKkhpX5fr1TcPm204Z",
+  "https://fwondivlfr.ufs.sh/f/xBKBguy6vNcXgodp3KZsthHaiYyMG5I0bpkzQefx6DwsSZ2V",
+  "https://fwondivlfr.ufs.sh/f/xBKBguy6vNcXzLM65DqFXsghTtYSQULR8k0Zrd2O7xnlwGIu",
+  "https://fwondivlfr.ufs.sh/f/xBKBguy6vNcXGG9kLbQ2Hrg3GD6dTxFCBiue5bal0JWtq9jh"
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
       Hello (gallery in progress)
     </main>
   );
